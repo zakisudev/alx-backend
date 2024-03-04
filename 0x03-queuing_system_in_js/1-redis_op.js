020d4d1ba427e42eaa8d6a670f0be3aafb738ecf
zakisu250 <zakisu250@gmail.com>
2024-03-04 10:40:38 +0300

    Add Redis operations script to print values

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+/**
+ * Author: Kwenziwa Lizwi Khanyile
+ * Github: @kwenziwa
+ * Date Created: February 1, 2024
+ */
+import redis from 'redis';
+
+const Newclient = redis.createClient();
+
+// when npm run dev 0-redis_client.js USE:
+// redis-7.2.4/src/redis-server > /dev/null 2>&1 & because
+// the src is in the redis-7.2.4 folder!
+Newclient.on('ready', () =>
+  console.log('Redis client connected to the server')
+).on('error', (err) =>
+  console.log(`Redis client not connected to the server: ${err}`)
+);
+
+const setNewSchool = (schoolName, value) => {
+  Newclient.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  Newclient.get(schoolName, (err, reply) => {
+    console.log(`${reply}`);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
