d871df1db645717d3e13a0bfefe33ef6385e1262
zakisu250 <zakisu250@gmail.com>
2024-03-04 10:50:46 +0300

    Refactor Redis advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,7 +1,5 @@
 /**
- * Author: Kwenziwa Lizwi Khanyile
- * Github: @kwenziwa
- * Date Created: February 1, 2024
+ * Redis advanced operations
  */
 import redis from 'redis';
 
@@ -21,7 +19,7 @@ const fields = {
   'New York': 20,
   Bogota: 20,
   Cali: 40,
-  Paris: 2
+  Paris: 2,
 };
 
 for (const [k, v] of Object.entries(fields)) {
