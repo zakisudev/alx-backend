997102b5196df7f64fdd77f5445042569cecd0f5
zakisu250 <zakisu250@gmail.com>
2024-03-04 10:55:25 +0300

    Add job processor for push notification code

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,18 @@
+/**
+ * Job Processor
+ */
+
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+};
+
+queue.process('push_notification_code', function (job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
